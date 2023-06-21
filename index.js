51efdd36c5a28c2fed8d121018768464ae7dbe1c
GianlucaCobucci <gianlucacobucci330@gmail.com>
2023-06-21 21:20:32 +0200

    created post model and added routes to root

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const morgan = require('morgan');
 
 const userRoute = require("./routes/users.js")
 const authRoute = require("./routes/auth.js")
-//const postRoute = require("./routes/posts.js")
+const postRoute = require("./routes/posts.js")
 
 dotenv.config();
 
@@ -31,7 +31,7 @@ app.use(morgan('common'));
 
 app.use("/api/users", userRoute)
 app.use("/api/auth", authRoute)
-//app.use("/api/posts", postRoute)
+app.use("/api/posts", postRoute)
 
 app.listen(PORT, () => {
     console.log(`Server avviato sulla porta ${PORT}`);
