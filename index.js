22429cecfa28b7c616a4e5a9b8a77e910713f257
GianlucaCobucci <gianlucacobucci330@gmail.com>
2023-07-03 17:56:06 +0200

    added multer for image upload

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const dotenv = require("dotenv");
 const helmet = require('helmet');
 const morgan = require('morgan');
 const cors = require('cors');
+const multer = require('multer');
 
 
 const userRoute = require("./routes/users.js")
@@ -18,6 +19,27 @@ app.use(cors());
 app.use(express.json())
 app.use(helmet());
 app.use(morgan('common'));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) =>{
+    cb(null, "public/assets")
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname)
+  }
+});
+
+const upload = multer({storage})
+app.post('/api/upload', upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).send({
+      message: "File caricato con successo",
+      statusCode: 200
+    })
+  } catch (error) {
+    console.log(error)
+  }
+})
 
 dotenv.config();
 
